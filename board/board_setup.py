239f10670881519593dfcc217bec6f0e9b09ca6f
Igor Beschastnov <beschastnovigor@gmail.com>
2023-05-03 15:35:30 +0300

    Add bitrate config

--- board/board_setup.py
+++ board/board_setup.py
@@ -9,17 +9,19 @@ def setup_can() -> bool:
 
     CAN_CONFIG = config["CAN"]
     CAN_TYPE = CAN_CONFIG["type"]
+    CAN_BITRATE = CAN_CONFIG.get("bitrate", "1000000")
+    CAN_DBITRATE = CAN_CONFIG.get("dbitrate", "8000000")
 
     if CAN_TYPE == "fd":
-        CAN_COMMAND = "ip link set {iface} up type can bitrate 1000000 dbitrate 8000000 restart-ms 1000 berr-reporting on fd on"
+        CAN_COMMAND = "ip link set {iface} up type can bitrate {bitrate} dbitrate {dbitrate} restart-ms 1000 berr-reporting on fd on"
     elif CAN_TYPE == "classic":
-        CAN_COMMAND = "ip link set {iface} up txqueuelen 65535 type can bitrate 1000000"
+        CAN_COMMAND = "ip link set {iface} up txqueuelen 65535 type can bitrate {bitrate} berr-reporting on"
     else:
         journal.send(f"Unknown CAN type {CAN_TYPE}")
         return False
 
     for iface in ("can0", "can1"):
-        cmd = CAN_COMMAND.format(iface=iface)
+        cmd = CAN_COMMAND.format(iface=iface, bitrate=CAN_BITRATE, dbitrate=CAN_DBITRATE)
         journal.send(f"Running {cmd}...")
         retcode = subprocess.call(cmd.split(" "))
         if retcode != 0:
--- install.mjs
+++ install.mjs
@@ -3,5 +3,5 @@
 let unit_dir = "/etc/systemd/system"
 
 await $`sudo mkdir -p ${unit_dir}`
-await $`sudo rm ${unit_dir}/board.service && sudo ln -s /opt/voltbro/board/board.service ${unit_dir}/board.service`
+await $`sudo rm -f ${unit_dir}/board.service && sudo ln -s /opt/voltbro/board/board.service ${unit_dir}/board.service`
 await $`sudo systemctl daemon-reload`
