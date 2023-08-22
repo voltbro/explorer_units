0614d853b15a62cffc12618473d307cc510c57b2
Igor Beschastnov <beschastnovigor@gmail.com>
2023-08-22 19:40:01 +0300

    signal handling

--- ros/scripts/cyphal_bridge.sh
+++ ros/scripts/cyphal_bridge.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source /opt/voltbro/ros/scripts/setup.sh
 
-roslaunch --wait cyphal_bridge cyphal_bridge.launch
+roslaunch --wait --sigterm-timeout=10 cyphal_bridge cyphal_bridge.launch
