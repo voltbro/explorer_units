489b3ed2dee3a7cb06d44361f8cb10a850caf526
Igor Beschastnov <beschastnovigor@gmail.com>
2023-10-02 19:16:44 +0300

    fix repo

--- download_and_install.sh
+++ download_and_install.sh
@@ -1,7 +1,7 @@
 sudo apt install python3-systemd
 sudo mkdir -p /opt/voltbro
-curl -L https://github.com/voltbro/vbcores-units/archive/refs/tags/0.3.0.tar.gz -o vbcores-units.tar.gz
-sudo sh -c "mkdir -p /opt/voltbro && tar -xvzf vbcores-units.tar.gz --directory /opt/voltbro --strip-components 1"
+curl -L https://github.com/voltbro/explorer_units/archive/refs/tags/0.3.0.tar.gz -o explorer_units.tar.gz
+sudo sh -c "mkdir -p /opt/voltbro && tar -xvzf explorer_units.tar.gz --directory /opt/voltbro --strip-components 1"
 sudo chown -R pi:root /opt/voltbro
 sudo chmod -R u+rwx,g+rwx,o+r /opt/voltbro
 
--- ros/scripts/setup.sh
+++ ros/scripts/setup.sh
@@ -1,2 +1,2 @@
 source /opt/ros/noetic/setup.sh
-source /home/pi/vbcores/ros/install/setup.sh
+source "${CONTROL_DIR:-/home/pi/explorer_control}/ros/install/setup.sh"
